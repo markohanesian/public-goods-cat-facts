e5e5ce8829438f19c5e33d9de8b2df46e8c59f1b
mso872 <mso872@gmail.com>
2021-12-30 21:46:31 -0800

    Converting API request to JSON data

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // get list of cats 
 fetch('https://cat-fact.herokuapp.com').then(function (response) {
     // successful api call
-    console.log('api call successful', response);
+    return response.json();
+}).then(function (data) {
+    // JSON data from call 
+    console.log(data)
 }).catch(function (err) {
     // error
     console.warn('api call error', err);
